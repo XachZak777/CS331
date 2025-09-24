4e8fdf74a56bec7a6eb51a2f292f951617f9318a
XachZak777 <khachik_zakaryan@edu.aua.am>
2025-09-24 14:51:22 +0000

    Adding hw files

---
+++ hw/hw4/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+    int num = 3;
+    int result = square(num);
+
+    printf("The square of %d = %d\n", num, result);
+
+    return 0;
+}
